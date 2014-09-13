94fef6eb33c8a26ad87c14ac06f53eaa2e56e272
Noitidart <noitidart@gmail.com>
2014-09-13 11:49:19 -0700

    initial.rev1
    added window listener

--- bootstrap.js
+++ bootstrap.js
@@ -8,6 +8,61 @@ const self = {
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/devtools/Console.jsm');
 
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener('load', function () {
+			aDOMWindow.removeEventListener('load', arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let DOMWindows = Services.wm.getEnumerator(null);
+		while (DOMWindows.hasMoreElements()) {
+			let aDOMWindow = DOMWindows.getNext();
+			windowListener.loadIntoWindow(aDOMWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let DOMWindows = Services.wm.getEnumerator(null);
+		while (DOMWindows.hasMoreElements()) {
+			let aDOMWindow = DOMWindows.getNext();
+			windowListener.unloadFromWindow(aDOMWindow);
+		}
+		
+		for (var u in unloaders) {
+			unloaders[u]();
+		}
+		
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		
+		aDOMWindow.addEventListener('activate', activated, false);		
+	},
+	unloadFromWindow: function (aDOMWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		
+		aDOMWindow.removeEventListener('activate', activated, false);
+	}
+};
+/*end - windowlistener*/
 
 function install() {}
 
@@ -15,8 +70,11 @@ function uninstall() {}
 
 function startup(aData, aReason) {
 	//self.aData = aData;
+	windowListener.register();
 }
  
 function shutdown(aReason) {
 	if (aReason == APP_SHUTDOWN) return;
+	
+	windowListener.unregister();
 }
